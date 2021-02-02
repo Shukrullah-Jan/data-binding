761e92beb074c563f71d9f4d5eecaf77b527bd6b
Shukrullah <Almehdipco6766@gmail.com>
2021-02-02 21:37:01 +0500

    Cleared the toast message

--- app/src/main/java/com/global/databinding/MainActivity.kt
+++ app/src/main/java/com/global/databinding/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.global.databinding
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.global.databinding.databinding.ActivityMainBinding
@@ -16,15 +15,12 @@ class MainActivity : AppCompatActivity() {
 
         activityMainBinding.tvData = "Hey it is very simple"
 
-        Toast.makeText(this, "hey", Toast.LENGTH_LONG).show()
         val user = User(
             "Kamal",
             23,
             true,
             "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"
         )
-
-        // this is only a comment and i am checking whether it is working or not.
         activityMainBinding.myUser = user
 
 
